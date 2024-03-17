4a24a998eb06f5c1cceccd64b7a3d9ba2b1a05b8
DiegoDeDios <diegomzdedios@gmail.com>
2024-03-17 16:59:12 -0600

    Fix CST timezone offset error

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import pytz
 from datetime import datetime
 
 def get_cst_time(ts):
-    cst = pytz.timezone('America/Chicago') #Hora local Mexico
+    cst = pytz.timezone('America/Mexico_City') #Hora local Mexico
     local_date =  datetime.fromtimestamp(ts, tz=pytz.utc).astimezone(cst)
     return local_date.strftime("%y/%m/%d %H:%M:%S")
 
